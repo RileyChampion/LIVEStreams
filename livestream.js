16ac7f3d94954f02b6b3e0b8b10ad8862be8662d
RileyChampion <rchamp2u@gmail.com>
2020-11-21 04:08:01 -0800

    All acquired livestreamers have been added to HTML! Now time for CSS

--- livestream.js
+++ livestream.js
@@ -43,7 +43,6 @@ async function getFollowedUsers(id) {
     await fetch(myRequest).then(function(response) {
         return response.json()
         .then(jsonResponse => {
-            console.log(jsonResponse.pagination)
             for (let i = 0; i <jsonResponse.data.length; i++){
                 follow_id.push(jsonResponse.data[i].to_id)
             }
@@ -79,15 +78,15 @@ async function constructStreamers() {
     }
 
     for (let i = 0; i < follow_id.length; i++) {
-        console.log('hahah')
         streamer_info = {}
         streamer_info['streamer_id'] = follow_id[i]
         //Get User Information
         let myRequest = new Request(`https://api.twitch.tv/helix/users?id=${follow_id[i]}`, myInit)
         await fetch(myRequest).then(function(response) {
-            return response.json()
+            response.json()
             .then(jsonResponse => {
-                streamer_info['streamer_name'] = jsonResponse.data[0].user_name
+                console.log(jsonResponse)
+                streamer_info['streamer_name'] = jsonResponse.data[0].display_name
                 streamer_info['streamer_profile_pic'] = jsonResponse.data[0].profile_image_url
             })
         })
@@ -96,7 +95,6 @@ async function constructStreamers() {
         await fetch(myRequest).then(function(response) {
             return response.json()
             .then(jsonResponse => {
-                console.log(jsonResponse.data)
                 if (jsonResponse.data.length !== 0){
                     streamer_info['stream_live'] = true
                     streamer_info['stream_title'] = jsonResponse.data[0].title
@@ -105,7 +103,7 @@ async function constructStreamers() {
                 }
                 else
                 {
-                    streamer_info['stream_live'] = true
+                    streamer_info['stream_live'] = false
                     streamer_info['stream_title'] = null
                     streamer_info['viewer_count'] = null
                     streamer_info['game_title'] = null
@@ -113,10 +111,45 @@ async function constructStreamers() {
             })
         })
 
-        streamer_info['stream_url'] = `https://twitch.tv/${streamer_info['streamer_name']}`
+        streamer_info['stream_url'] = `https://twitch.tv/${streamer_info['streamer_name'].toLowerCase()}`
 
         streamers.push(streamer_info)
     }   
+}
+
+function insertStreamer(streamer) {
+    content = document.getElementById("main-content")
+        var streamer_box = document.createElement("div")
+        streamer_box.setAttribute("id", "streamer");
+        streamer_box.setAttribute("class", "streamer");
+
+        var streamer_profile_pic = document.createElement("img")
+        streamer_profile_pic.setAttribute("class", "pfp");
+        streamer_profile_pic.setAttribute("src", `${streamer.streamer_profile_pic}`);
+        streamer_box.appendChild(streamer_profile_pic)
+
+        var streamers_name = document.createElement("p")
+        streamers_name.setAttribute("class", "streamer-name");
+        streamers_name.innerHTML = `${streamer.streamer_name}`;
+        streamer_box.appendChild(streamers_name);
+
+        var streamers_game = document.createElement("p")
+        streamers_game.setAttribute("class", "streamer-game");
+        streamers_game.innerHTML = `Game: ${streamer.game_name == null ? "N/A" : streamers[i].game_name}`
+        streamer_box.appendChild(streamers_game);
+
+        var streamer_url = document.createElement("a");
+        streamer_url.setAttribute("class", "arrow-link");
+        streamer_url.setAttribute("href", `${streamer.stream_url}`);
+        streamer_url.setAttribute("target", "_blank");
+        var arrow_icon = document.createElement("img");
+        arrow_icon.setAttribute("src", "icons/caret-right-solid.svg");
+        arrow_icon.setAttribute("height", "87");
+        arrow_icon.setAttribute("height", "100");
+        streamer_url.appendChild(arrow_icon);
+        streamer_box.appendChild(streamer_url);
+
+        content.appendChild(streamer_box);
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
@@ -124,4 +157,21 @@ document.addEventListener('DOMContentLoaded', async () => {
     const userID = await getUserId() //async and await is nw standard JS 
     await getFollowedUsers(userID)
     await constructStreamers()
+    console.log(streamers)
+    /*
+    <!-- <div id="streamer" class="streamer">
+            <img class="pfp" src="">
+            <p class="streamer-name">Streamer</p>
+            <p class="streamer-game">Game: </p>
+            <div class="live-count">
+                <img class="viewer-icon">
+                <p class="viwer-number">1000</p>
+            </div>
+            <a class="arrow-link" href=""><img src=""></a>
+        </div> -->
+    */
+    document.getElementById("search-box").style.display = "none";
+    for (let i = 0; i < streamers.length; i++) {
+        insertStreamer(streamers[i])
+    }
 })
